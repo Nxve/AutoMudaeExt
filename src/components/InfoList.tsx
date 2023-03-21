38bed2b08df0064ad9e7f4b9c21db1d5e3ca390a
Nxve <danielmcolaco@outlook.com>
2023-03-21 17:37:09 -0300

    InfoList minHeight + auto scroll

--- src/components/InfoList.tsx
+++ src/components/InfoList.tsx
@@ -1,6 +1,7 @@
 import type { BotLog } from "../lib/events";
 import type { InfoPanelType } from "../lib/app_types"
 import LogItem from "./LogItem";
+import { useEffect, useRef } from "react";
 
 interface IInfoListProps {
     infoType: NonNullable<InfoPanelType>
@@ -8,8 +9,14 @@ interface IInfoListProps {
 }
 
 function InfoList({ infoType, logs }: IInfoListProps) {
+    const refList = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        refList.current?.scrollBy(0, refList.current.scrollHeight);
+    }, []);
+
     return (
-        <div id="info-list" className={`list ${infoType}`}>
+        <div id="info-list" className={`list ${infoType}`} ref={refList}>
             {
                 logs.map((log, i) =>
                     <LogItem log={log} key={`log-${i}`} />
